96029ce29d862a978d9cc8e04f27e04f4893426d
spicious225 <ryanthelion.c@gmail.com>
2023-09-12 00:32:30 -0700

    improved ui on home page

--- js/loadContent.js
+++ js/loadContent.js
@@ -102,6 +102,17 @@ function setupInteractiveElements() {
   });
 }
 
+document.addEventListener("mousemove", function(e) {
+  const header = document.querySelector(".navbar");
+  // If the mouse is within 50 pixels from the top of the viewport, show the header
+  if (e.clientY <= 50) {
+      header.style.opacity = "1";
+  } else {
+      header.style.opacity = "0";
+  }
+});
+
+
 function populateTable() {
   const tableBody = document.querySelector("#linksTable tbody");
   tableBody.innerHTML = '';  // Clear the existing rows
