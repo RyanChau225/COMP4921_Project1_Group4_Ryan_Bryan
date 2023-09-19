3c43eb3d697fe12f95e336d56d22e2f92223074e
Bryan Tran <bryant.business@hotmail.com>
2023-09-18 18:35:06 -0700

    add env and connect mongodb

--- index.js
+++ index.js
@@ -1,273 +1,279 @@
-require('./utils');
-
-// require('dotenv').config();
+require('dotenv').config();
+// const PORT = 3000;
 const express = require('express');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo');
-// const bcrypt = require('bcrypt');
-const saltRounds = 12;
-
-
-// const database = include('databaseConnection');
-// const db_utils = include('database/db_utils');
-// const db_users = include('database/users');
-// const success = db_utils.printMySQLVersion();
-
-const port = process.env.PORT || 3000;
-
 const app = express();
-
-const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
-
-
-/* secret information section */
-// const mongodb_user = process.env.MONGODB_USER;
-// const mongodb_password = process.env.MONGODB_PASSWORD;
-// const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-
-// const node_session_secret = process.env.NODE_SESSION_SECRET;
-// /* END secret section */
-
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const bcrypt = require('bcrypt');
+const {
+    render
+} = require('express/lib/response');
+var session = require('express-session');
+const req = require('express/lib/request');
 app.set('view engine', 'ejs');
 
-// app.use(express.urlencoded({extended: false}));
 
-// var mongoStore = MongoStore.create({
-// 	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@cluster0.fuu9a.mongodb.net/sessions`,
-// 	crypto: {
-// 		secret: mongodb_session_secret
-// 	}
-// })
+const mongodb_host = process.env.REMOTE_MONGODB_HOST;
+const mongodb_user = process.env.REMOTE_MONGODB_USER;
+const mongodb_password = process.env.REMOTE_MONGODB_PASSWORD;
 
-// app.use(session({ 
-//     secret: node_session_secret,
-// 	store: mongoStore, //default is memory store 
-// 	saveUninitialized: false, 
-// 	resave: true
-// }
-// ));
+app.use(bodyparser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
+    extended: true
+}));
 
-app.get('/', (req,res) => {
-    res.render("index");
-});
+// Use the session middleware
+app.use(session({
+    secret: "hello, world",
+    saveUninitialized: true,
+    resave: true
+}));
 
-app.get('/about', (req,res) => {
-    var color = req.query.color;
-    if (!color) {
-        color = "black";
-    }
-
-    res.render("about", {color: color} );
-});
-
-app.get('/contact', (req,res) => {
-    var missingEmail = req.query.missing;
-    res.render("contact", {missing: missingEmail});
-});
-
-app.post('/submitEmail', (req,res) => {
-    var email = req.body.email;
-    if (!email) {
-        res.redirect('/contact?missing=1');
-    }
-    else {
-        res.render("submitEmail", {email: email});
-    }
-});
-
-app.get('/createTables', async (req,res) => {
-
-    const create_tables = include('database/create_tables');
-
-    var success = create_tables.createTables();
-    if (success) {
-        res.render("successMessage", {message: "Created tables."} );
-    }
-    else {
-        res.render("errorMessage", {error: "Failed to create tables."} );
-    }
-});
-
-app.get('/createUser', (req,res) => {
-    res.render("createUser");
+// Connect mongoose to server
+mongoose.connect(`mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 
-app.get('/login', (req,res) => {
-    res.render("login");
-});
-
-app.get('/signup', (req,res) => {
-    res.render("signup");
-});
-
-app.post('/submitUser', async (req,res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-
-    var hashedPassword = bcrypt.hashSync(password, saltRounds);
-
-    var success = await db_users.createUser({ user: username, hashedPassword: hashedPassword });
-
-    if (success) {
-        var results = await db_users.getUsers();
-
-        res.render("submitUser",{users:results});
-    }
-    else {
-        res.render("errorMessage", {error: "Failed to create user."} );
-    }
-
-});
-
-app.post('/loggingin', async (req,res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-
-
-    var results = await db_users.getUser({ user: username, hashedPassword: password });
-
-    if (results) {
-        if (results.length == 1) { //there should only be 1 user in the db that matches
-            if (bcrypt.compareSync(password, results[0].password)) {
-                req.session.authenticated = true;
-                req.session.user_type = results[0].type; 
-                req.session.username = username;
-                req.session.cookie.maxAge = expireTime;
-        
-                res.redirect('/loggedIn');
-                return;
-            }
-            else {
-                console.log("invalid password");
-            }
-        }
-        else {
-            console.log('invalid number of users matched: '+results.length+" (expected 1).");
-            res.redirect('/login');
-            return;            
-        }
-    }
-
-    console.log('user not found');
-    //user and password combination not found
-    res.redirect("/login");
-});
-
-
-function isValidSession(req) {
-	if (req.session.authenticated) {
-		return true;
-	}
-	return false;
-}
-
-function sessionValidation(req, res, next) {
-	if (!isValidSession(req)) {
-		req.session.destroy();
-		res.redirect('/login');
-		return;
-	}
-	else {
-		next();
-	}
-}
-
-function isAdmin(req) {
-    if (req.session.user_type == 'admin') {
-        return true;
-    }
-    return false;
-}
-
-function adminAuthorization(req, res, next) {
-	if (!isAdmin(req)) {
-        res.status(403);
-        res.render("errorMessage", {error: "Not Authorized"});
-        return;
-	}
-	else {
-		next();
-	}
-}
-
-app.use('/loggedin', sessionValidation);
-app.use('/loggedin/admin', adminAuthorization);
-
-app.get('/loggedin', (req,res) => {
-    res.render("loggedin");
-});
-
-app.get('/loggedin/info', (req,res) => {
-    res.render("loggedin-info");
-});
-
-app.get('/loggedin/admin', (req,res) => {
-    res.render("admin");
-});
-
-app.get('/loggedin/memberinfo', (req,res) => {
-    res.render("memberInfo", {username: req.session.username, user_type: req.session.user_type});
-});
-
-
-app.get('/cat/:id', (req,res) => {
-    var cat = req.params.id;
-
-    res.render("cat", {cat: cat});
-});
-
-
-app.get('/api', (req,res) => {
-	var user = req.session.user;
-    var user_type = req.session.user_type;
-	console.log("api hit ");
-
-	var jsonResponse = {
-		success: false,
-		data: null,
-		date: new Date()
-	};
-
-	
-	if (!isValidSession(req)) {
-		jsonResponse.success = false;
-		res.status(401);  //401 == bad user
-		res.json(jsonResponse);
-		return;
-	}
-
-	if (typeof id === 'undefined') {
-		jsonResponse.success = true;
-		if (user_type === "admin") {
-			jsonResponse.data = ["A","B","C","D"];
-		}
-		else {
-			jsonResponse.data = ["A","B"];
-		}
-	}
-	else {
-		if (!isAdmin(req)) {
-			jsonResponse.success = false;
-			res.status(403);  //403 == good user, but, user should not have access
-			res.json(jsonResponse);
-			return;
-		}
-		jsonResponse.success = true;
-		jsonResponse.data = [id + " - details"];
-	}
-
-	res.json(jsonResponse);
-
-});
-
-app.use(express.static(__dirname + "/public"));
-
-app.get("*", (req,res) => {
-	res.status(404);
-	res.render("404");
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    admin: Boolean
 })
 
-app.listen(port, () => {
-	console.log("Node application listening on port "+port);
-}); 
+const userModel = mongoose.model("users", userSchema);
+
+
+
+
+// instead of using app.get() for every file, use this middleware instead. It serves all the required files to the client for you.
+app.use(express.static('public'));
+
+
+// Route to render login.ejs when website is initially visited.
+app.get('/', (req, res) => {
+    res.render('login.ejs');
+})
+
+
+
+// Render login.ejs.
+app.get('/login', (req, res) => {
+    res.render("login.ejs");
+})
+
+// Render signup.ejs
+app.get('/signup', (req, res) => {
+    res.render("signup.ejs");
+})
+
+// Route that sends all user data to client
+app.get('/loadAllUsers', (req, res) => {
+    userModel.find({}, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(data);
+        }
+    })
+})
+
+// Check if the username exists in the database.
+// I need to do this callback fuction thing only in server.js for some reason. I googled it and I don't understand why.
+function isUsernameInDb(user, callback) {
+    userModel.find({
+        username: user
+    }, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+
+        return callback(data.length != 0);
+    })
+}
+
+// Signs up the user. Stores their credentials into the database. Then adds the user into the session.
+app.post('/userSignUp', (req, res) => {
+    let isUsernameInDbVariable;
+    // I need to do this callback fuction thing only in server.js for some reason. I googled it and I don't understand why.
+    isUsernameInDb(req.body.username, (response) => {
+        isUsernameInDbVariable = response;
+    })
+
+    const plaintextPassword = req.body.password;
+    const saltRounds = 10;
+
+    bcrypt.hash(plaintextPassword, saltRounds, (err, hash) => {
+        if (!isUsernameInDbVariable) {
+            userModel.create({
+                username: req.body.username,
+                password: hash,
+                name: req.body.name,
+                admin: false
+            }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                }
+
+                // Login user via express-session
+                req.session.authenticated = true;
+                req.session.username = data.username;
+                req.session.uid = data._id;
+
+                res.send(data.name);
+            })
+        } else {
+            res.send(true);
+        }
+    })
+})
+
+// Adds a users into the databse. only for admin.
+app.post('/addUser', (req, res) => {
+    let isUsernameInDbVariable;
+    // I need to do this callback fuction thing only in server.js for some reason. I googled it and I don't understand why.
+    isUsernameInDb(req.body.username, (response) => {
+        isUsernameInDbVariable = response;
+    })
+
+    const plaintextPassword = req.body.password;
+    const saltRounds = 10;
+
+    bcrypt.hash(plaintextPassword, saltRounds, (err, hash) => {
+        if (!isUsernameInDbVariable) {
+            userModel.create({
+                username: req.body.username,
+                password: hash,
+                name: req.body.name,
+                admin: req.body.admin
+            }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                }
+
+                res.send(data.name);
+            })
+        } else {
+            res.send(true);
+        }
+    })
+})
+
+app.post('/checkUserCredentials', (req, res) => {
+    let usernameToCheck = req.body.username;
+    let passwordToCheck = req.body.password; // This password is in plaintext.
+    let expectedHashedPassword = "";
+
+    userModel.find({
+        username: usernameToCheck
+    }, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (data.length > 0) {
+                expectedHashedPassword = data[0].password;
+            }
+
+
+            bcrypt.compare(passwordToCheck, expectedHashedPassword, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else if (result) {
+                    req.session.authenticated = true;
+                    req.session.username = data[0].username;
+                    req.session.uid = data[0]._id;
+    
+                    res.send(true);
+                } else {
+                    res.send(false);
+                }
+            })
+        }
+    })
+})
+
+// This route updates the user info.
+app.post('/updateUser', (req, res) => {
+    let admin;
+
+    // Swaps format of admin back to database format.
+    if (req.body.admin.toLowerCase() == "yes") {
+        admin = true;
+    } else {
+        admin = false;
+    }
+
+    userModel.updateOne({
+        _id: req.body._id
+    }, {
+        username: req.body.username,
+        name: req.body.name,
+        admin: admin
+    }, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(data);
+        }
+    })
+})
+
+// Route to delete user.
+app.post('/deleteUser', (req, res) => {
+    if (req.body._id == req.session.uid) {
+        res.send(false);
+    } else {
+        userModel.deleteOne({
+            _id: req.body._id
+        }, (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(true);
+            }
+        })
+    }
+
+
+})
+
+app.get('/isUserAnAdmin', (req, res) => {
+    let loggedInUser = req.session.username;
+
+    userModel.find({
+        username: loggedInUser
+    }, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(data[0].admin);
+        }
+    })
+})
+
+// Check if user is logged in.
+app.get('/isUserLoggedIn', (req, res) => {
+    if (req.session.authenticated) {
+        res.send(true);
+    } else {
+        res.send(false);
+    }
+})
+
+// Ends the user session.
+app.get('/logout', (req, res) => {
+    req.session.authenticated = false;
+    req.session.username = undefined;
+    req.session.uid = undefined;
+
+    res.render('login.ejs');
+})
+
+
+app.listen(process.env.PORT || 3000, function (err) {
+    if (err)
+        console.log(err);
+})
