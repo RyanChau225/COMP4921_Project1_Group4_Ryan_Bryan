5d6b1cf2c263015322b59bddcb5128997caad00e
Bryan Tran <bryant.business@hotmail.com>
2023-09-24 21:56:35 -0700

    signup is all good to go

--- routes/router.js
+++ routes/router.js
@@ -55,20 +55,14 @@ const secret_token = process.env.SECRET_TOKEN
 //     extended: true
 // }));
 
-const db = mongoose.connection;
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-db.once('open', () => {
-  console.log('MongoDB connected successfully');
-});
+// // Use the session middleware
+// router.use(session({
+//     secret: `${secret_token}`,
+//     saveUninitialized: true,
+//     resave: true
+// }));
 
-// Check the connection state
-if (mongoose.connection.readyState === 1) {
-  console.log('Mongoose is connected to MongoDB');
-} else {
-  console.log('Mongoose is not connected to MongoDB');
-}
+
 
 router.get('/', async (req, res) => {
 	console.log("page hit");
@@ -97,6 +91,10 @@ router.get('/', async (req, res) => {
 		console.log(ex);
 	}
 });
+
+
+
+
 
 router.get('/pic', async (req, res) => {
 	  res.send('<form action="picUpload" method="post" enctype="multipart/form-data">'
@@ -214,40 +212,6 @@ router.get('/showMedia', async (req, res) => {
 	}
 });
 
-// router.get('/deleteUser', async (req, res) => {
-// 	try {
-// 		console.log("delete user");
-
-// 		let user_id = req.query.id;
-
-// 		const schema = Joi.object(
-// 			{
-// 				user_id: Joi.string().alphanum().min(24).max(24).required()
-// 			});
-		
-// 		const validationResult = schema.validate({user_id});
-// 		if (validationResult.error != null) {
-// 			console.log(validationResult.error);
-
-// 			res.render('error', {message: 'Invalid user_id'});
-// 			return;
-// 		}				
-
-// 		if (user_id) {
-// 			console.log("userId: "+user_id);
-// 			const result1 = await mediaCollection.deleteMany({"user_id": new ObjectId(user_id)});
-// 			const result2 = await userCollection.deleteOne({"_id": new ObjectId(user_id)});
-
-// 			console.log("deleteUser: ");
-// 		}
-// 		res.redirect("/");
-// 	}
-// 	catch(ex) {
-// 		res.render('error', {message: 'Error connecting to MongoDB'});
-// 		console.log("Error connecting to MongoDB");
-// 		console.log(ex);	
-// 	}
-// });
 
 router.get('/deletemediaImage', async (req, res) => {
 	try {
@@ -333,10 +297,13 @@ router.post('/addUser', async (req, res) => {
 		res.redirect("/");
 	}
 	catch(ex) {
-		res.render('error', {message: 'Error connecting to MySQL'});
-		console.log("Error connecting to MySQL");
+		if(ex.code == 11000){
+			res.render('error', {message: `No duplicates`});
+		}
+		else{
+		res.render('Message', {message: `${ex}`});
 		console.log(ex);	
-	}
+	}}
 });
 
 
@@ -381,28 +348,6 @@ router.post('/addmedia', async (req, res) => {
 });
 
 
-// // Use the session middleware
-// router.use(session({
-//     secret: `${secret_token}`,
-//     saveUninitialized: true,
-//     resave: true
-// }));
-
-// // Connect mongoose to server
-// mongoose.connect(`mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}?retryWrites=true&w=majority`, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-
-// const userSchema = new mongoose.Schema({
-//     username: String,
-//     password: String,
-//     name: String,
-//     admin: Boolean
-// })
-
-// const userModel = mongoose.model("users", userSchema);
 
 
 // Render login.ejs.
@@ -420,212 +365,12 @@ router.get('/home', (req, res) => {
     res.render("home.ejs");
 })
 
-// Route that sends all user data to client
-// router.get('/loadAllUsers', (req, res) => {
-//     userModel.find({}, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             res.send(data);
-//         }
-//     })
-// })
-
-// // Check if the username exists in the database.
-// // I need to do this callback fuction thing only in server.js for some reason. I googled it and I don't understand why.
-// function isUsernameInDb(user, callback) {
-//     userModel.find({
-//         username: user
-//     }, (err, data) => {
-//         if (err) {
-//             console.log(err)
-//         }
-
-//         return callback(data.length != 0);
-//     })
-// }
-
-// // Signs up the user. Stores their credentials into the database. Then adds the user into the session.
-// router.post('/userSignUp', (req, res) => {
-//     let isUsernameInDbVariable;
-//     // I need to do this callback fuction thing only in server.js for some reason. I googled it and I don't understand why.
-//     isUsernameInDb(req.body.username, (response) => {
-//         isUsernameInDbVariable = response;
-//     })
-
-//     const plaintextPassword = req.body.password;
-//     const saltRounds = 10;
-
-//     bcrypt.hash(plaintextPassword, saltRounds, (err, hash) => {
-//         if (!isUsernameInDbVariable) {
-//             userModel.create({
-//                 username: req.body.username,
-//                 password: hash,
-//                 name: req.body.name,
-//                 admin: false
-//             }, (err, data) => {
-//                 if (err) {
-//                     console.log(err);
-//                 }
-
-//                 // Login user via express-session
-//                 req.session.authenticated = true;
-//                 req.session.username = data.username;
-//                 req.session.uid = data._id;
-
-//                 res.send(data.name);
-//             })
-//         } else {
-//             res.render("index.ejs");
-//         }
-//     })
-// })
-
-// // Adds a users into the databse. only for admin.
-// app.post('/addUser', (req, res) => {
-//     let isUsernameInDbVariable;
-//     // I need to do this callback fuction thing only in server.js for some reason. I googled it and I don't understand why.
-//     isUsernameInDb(req.body.username, (response) => {
-//         isUsernameInDbVariable = response;
-//     })
-
-//     const plaintextPassword = req.body.password;
-//     const saltRounds = 10;
-
-//     bcrypt.hash(plaintextPassword, saltRounds, (err, hash) => {
-//         if (!isUsernameInDbVariable) {
-//             userModel.create({
-//                 username: req.body.username,
-//                 password: hash,
-//                 name: req.body.name,
-//                 admin: req.body.admin
-//             }, (err, data) => {
-//                 if (err) {
-//                     console.log(err);
-//                 }
-
-//                 res.send(data.name);
-//             })
-//         } else {
-//             res.send(true);
-//         }
-//     })
-// })
-
-// router.post('/checkUserCredentials', (req, res) => {
-//     let usernameToCheck = req.body.username;
-//     let passwordToCheck = req.body.password; // This password is in plaintext.
-//     let expectedHashedPassword = "";
-
-//     userModel.find({
-//         username: usernameToCheck
-//     }, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             if (data.length > 0) {
-//                 expectedHashedPassword = data[0].password;
-//             }
 
 
-//             bcrypt.compare(passwordToCheck, expectedHashedPassword, (err, result) => {
-//                 if (err) {
-//                     console.log(err);
-//                 } else if (result) {
-//                     req.session.authenticated = true;
-//                     req.session.username = data[0].username;
-//                     req.session.uid = data[0]._id;
-    
-//                     res.send(true);
-//                 } else {
-//                     res.send(false);
-//                 }
-//             })
-//         }
-//     })
-// })
 
-// // This route updates the user info.
-// router.post('/updateUser', (req, res) => {
-//     let admin;
-
-//     // Swaps format of admin back to database format.
-//     if (req.body.admin.toLowerCase() == "yes") {
-//         admin = true;
-//     } else {
-//         admin = false;
-//     }
-
-//     userModel.updateOne({
-//         _id: req.body._id
-//     }, {
-//         username: req.body.username,
-//         name: req.body.name,
-//         admin: admin
-//     }, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             res.send(data);
-//         }
-//     })
-// })
-
-// Route to delete user.
-// router.post('/deleteUser', (req, res) => {
-//     if (req.body._id == req.session.uid) {
-//         res.send(false);
-//     } else {
-//         userModel.deleteOne({
-//             _id: req.body._id
-//         }, (err, data) => {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 res.send(true);
-//             }
-//         })
-//     }
-
-
-// })
-
-// router.get('/isUserAnAdmin', (req, res) => {
-//     let loggedInUser = req.session.username;
-
-//     userModel.find({
-//         username: loggedInUser
-//     }, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             res.send(data[0].admin);
-//         }
-//     })
-// })
-
-// // Check if user is logged in.
-// router.get('/isUserLoggedIn', (req, res) => {
-//     if (req.session.authenticated) {
-//         res.send(true);
-//     } else {
-//         res.send(false);
-//     }
-// })
-
-// // Ends the user session.
-// router.get('/logout', (req, res) => {
-//     req.session.authenticated = false;
-//     req.session.username = undefined;
-//     req.session.uid = undefined;
-
-//     res.render('login.ejs');
-// })
-
-
-// router.get("*", (req,res) => {
-// 	res.status(404);
-// 	res.render("404");
-// })
+router.get("*", (req,res) => {
+	res.status(404);
+	res.render("404");
+})
 
 module.exports = router;
